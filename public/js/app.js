e9d7e37cf5e9c085c2466d4829d70db4a4e2b0a9
Olutobi Adeyemi <olutobiadeyemi@MACBOOKs-MacBook-Pro.local>
2017-06-17 19:36:07 +0100

    Added props to Product component

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,19 @@
 
 class ProductList extends React.Component {
     render() {
+        const product = Seed.products[0];
         return (
             <div className="ui unstackable items">
                 Hello, friend! I am a basic React Component
-                <Product />
+                <Product 
+                    id={product.id}
+                    title={product.title}
+                    description={product.description}
+                    url={product.url}
+                    votes={product.votes}
+                    submitterAvatarUrl={product.submitterAvatarUrl}
+                    productImageUrl={product.productImageUrl}
+                />
             </div>
         );
     }
@@ -15,17 +24,27 @@ class Product extends React.Component {
         return (
             <div className="item">
                 <div className="image">
-                    <img src="images/products/image-aqua.png"/>
+                    <img src={this.props.productImageUrl}/>
                 </div>
                 <div className="middle aligned content">
+                    <div className="header">
+                        <a>
+                            <i className="large caret up icon"></i>
+                        </a>
+                        {this.props.votes}
+                    </div>
                     <div className="description">
-                        <a>Fort Knight</a>
-                        <p>Authentic renaissance actors, delivered in just two weeks</p>
+                        <a href={this.props.url}>
+                            {this.props.title}
+                        </a>
+                        <p>
+                            {this.props.description}
+                        </p>
                     </div>
                     <div className="extra">
                         <span>Submitted by:</span>
                         <img 
-                            src="images/avatars/daniel.jpg" 
+                            src={this.props.submitterAvatarUrl} 
                             className="ui avatar image"
                         />
                     </div>
